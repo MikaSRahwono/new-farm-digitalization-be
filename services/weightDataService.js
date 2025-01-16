52e5e30f44bf896c8c8950ae57923707bf691884
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2025-01-17 06:36:22 +0700

    [FIX] Fixing weightData service

--- services/weightDataService.js
+++ services/weightDataService.js
@@ -10,31 +10,26 @@ const { WeightData, YearlyData, MonthlyData } = require('../models');
 const createWeightData = async (animalId, yearlyData) => {
     const weight = await WeightData.create({ animalId });
 
-    await Promise.all(weight.yearlyDatas.map(async (yearlyDataEntry) => {
-    await MonthlyData.destroy({ where: { yearlyDataId: yearlyDataEntry.id } });
-    await YearlyData.destroy({ where: { id: yearlyDataEntry.id } });
-    }));
-
     const yearlyDataEntries = await Promise.all(yearlyData.map(async (yData) => {
-    const yearlyEntry = await YearlyData.create({
-        year: yData.year,
-        conditionType: "WeightData",
-        conditionId: weight.id,
-    });
+        const yearlyEntry = await YearlyData.create({
+            year: yData.year,
+            conditionType: "WeightData",
+            conditionId: weight.id,
+        });
 
-    const monthData = yData.data.map(mData => ({
+        const monthData = yData.data.map(mData => ({
         month: mData.month,
         value: mData.value,
         yearlyDataId: yearlyEntry.id,
+        }));
+
+        await MonthlyData.bulkCreate(monthData);
+        return yearlyEntry;
     }));
 
-    await MonthlyData.bulkCreate(monthData);
-    return yearlyEntry;
-    }));
-
-    weight.yearlyDatas = yearlyDataEntries;
     return weight;
 };
+
   
 /**
  * Get WeightData by Animal ID
@@ -62,37 +57,32 @@ const getWeightDataByAnimalId = async (animalId) => {
  * @returns {Promise<WeightData>}
  */
 const updateWeightData = async (id, yearlyData) => {
-  const lactation = await WeightData.findByPk(id, {
-    include: [{
-      model: YearlyData,
-      as: 'yearlyDatas',
-      include: [{ model: MonthlyData, as: 'monthlyDatas' }],
-    }],
-  });
+    const weight = await WeightData.create({ animalId });
 
-  if (!lactation) return null;
-
-  // Delete existing YearlyData and MonthlyData
-  await YearlyData.destroy({ where: { animalId: lactation.animalId } });
-
-  // Recreate YearlyData and MonthlyData
-  const yearlyDataEntries = yearlyData.map(yData => ({
-    year: yData.year,
-    animalId: lactation.animalId,
-  }));
-
-  const createdYearlyDatas = await YearlyData.bulkCreate(yearlyDataEntries, { returning: true });
-
-  for (let i = 0; i < createdYearlyDatas.length; i++) {
-    const monthData = yearlyData[i].data.map(mData => ({
-      month: mData.month,
-      value: mData.value,
-      yearlyDataId: createdYearlyDatas[i].id,
+    await Promise.all(weight.yearlyDatas.map(async (yearlyDataEntry) => {
+      await MonthlyData.destroy({ where: { yearlyDataId: yearlyDataEntry.id } });
+      await YearlyData.destroy({ where: { id: yearlyDataEntry.id } });
     }));
-    await MonthlyData.bulkCreate(monthData);
-  }
-
-  return lactation;
+  
+    const yearlyDataEntries = await Promise.all(yearlyData.map(async (yData) => {
+      const yearlyEntry = await YearlyData.create({
+        year: yData.year,
+        conditionType: "WeightData",
+        conditionId: weight.id,
+      });
+  
+      const monthData = yData.data.map(mData => ({
+        month: mData.month,
+        value: mData.value,
+        yearlyDataId: yearlyEntry.id,
+      }));
+  
+      await MonthlyData.bulkCreate(monthData);
+      return yearlyEntry;
+    }));
+  
+    weight.yearlyDatas = yearlyDataEntries;
+    return weight;
 };
 
 /**
