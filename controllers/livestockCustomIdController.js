41a327d9b9dead8d907ebd4fba50184b251c6c8b
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2025-01-17 13:46:45 +0700

    [FEAT] Adding template response

--- controllers/livestockCustomIdController.js
+++ controllers/livestockCustomIdController.js
@@ -12,12 +12,12 @@ class LivestockCustomIdController {
       const customId = await LivestockCustomIdService.getCustomIdByFarmAndType(farmId, typeId);
 
       if (!customId) {
-        return res.status(404).json({ message: 'Custom ID not found' });
+        return res.status(404).json({ success: false, message: 'Custom ID not found' });
       }
 
-      res.json(customId);
+      res.json({success: true, data: customId});
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ success: false, message: error.message });
     }
   }
 
@@ -32,9 +32,9 @@ class LivestockCustomIdController {
 
       const customId = await LivestockCustomIdService.createCustomId(farmId, typeId, customPrefix);
 
-      res.status(201).json(customId);
+      res.status(201).json({success: true, data: customId});
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ success: false, message: error.message });
     }
   }
 
@@ -49,9 +49,9 @@ class LivestockCustomIdController {
 
       const customId = await LivestockCustomIdService.incrementLastNumber(farmId, typeId);
 
-      res.json(customId);
+      res.json({success: true, data: customId});
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({success: false,  message: error.message });
     }
   }
 
@@ -67,12 +67,12 @@ class LivestockCustomIdController {
         const updated = await LivestockCustomIdService.changePrefix(farmId, typeId, newPrefix);
 
         if (!updated) {
-            return res.status(404).json({ message: 'Custom ID not found' });
+            return res.status(404).json({ success: false, message: 'Custom ID not found' });
         }
 
-        return res.status(200).json({ message: 'Prefix updated successfully and last_number reset to 0' });
+        return res.status(200).json({ success: true, message: 'Prefix updated successfully and last_number reset to 0' });
         } catch (error) {
-        return res.status(500).json({ error: 'Internal server error', details: error.message });
+        return res.status(500).json({ success: false, error: 'Internal server error', details: error.message });
         }
     }
 }
