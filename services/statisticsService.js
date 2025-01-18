1e7cd8d32cffd37859b4a99decb2d44f07d5a0fb
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2025-01-18 18:09:41 +0700

    [FEAT] Adding Milk and Lactation Data

--- services/statisticsService.js
+++ services/statisticsService.js
@@ -1,42 +1,60 @@
 // services/statisticsService.js
 
-const { Animal } = require('../models'); // Make sure the Animal model is properly imported
+const { Animal, LactationData, MilkData, YearlyData, MonthlyData } = require('../models');
 
 class StatisticsService {
   async getCowStatistics(farmId) {
     const cows = await Animal.findAll({
       where: {
         category: 'Sapi',
-        farmId: farmId,  // Filter by farmId
+        farmId: farmId, 
       },
     });
 
-    const stats = await this.calculateFarmStatistics(cows); // Calculate statistics for the specified farm
-    return { success: true, data: stats }; // Return response in the desired format
+    const stats = await this.calculateFarmStatistics(cows);
+    const lactationData = await this.getAnimalCategorySummaryLactationData("Sapi")
+    const milkData = await this.getAnimalCategorySummaryMilkData("Sapi")
+    return { success: true, data: {
+      summary: stats,
+      lactationData: lactationData,
+      milkData: milkData
+    } }; 
   }
 
   async getGoatStatistics(farmId) {
     const goats = await Animal.findAll({
       where: {
         category: 'Kambing',
-        farmId: farmId,  // Filter by farmId
+        farmId: farmId,  
       },
     });
 
-    const stats = await this.calculateFarmStatistics(goats); // Calculate statistics for the specified farm
-    return { success: true, data: stats }; // Return response in the desired format
+    const stats = await this.calculateFarmStatistics(goats);
+    const lactationData = await this.getAnimalCategorySummaryLactationData("Kambing")
+    const milkData = await this.getAnimalCategorySummaryMilkData("Kambing")
+    return { success: true, data: {
+      summary: stats,
+      lactationData: lactationData,
+      milkData: milkData
+    } }; 
   }
 
   async getSheepStatistics(farmId) {
     const sheep = await Animal.findAll({
       where: {
         category: 'Domba',
-        farmId: farmId,  // Filter by farmId
+        farmId: farmId,
       },
     });
 
-    const stats = await this.calculateFarmStatistics(sheep); // Calculate statistics for the specified farm
-    return { success: true, data: stats }; // Return response in the desired format
+    const stats = await this.calculateFarmStatistics(sheep);
+    const lactationData = await this.getAnimalCategorySummaryLactationData("Domba")
+    const milkData = await this.getAnimalCategorySummaryMilkData("Domba")
+    return { success: true, data: {
+      summary: stats,
+      lactationData: lactationData,
+      milkData: milkData
+    } }; 
   }
 
   /**
@@ -52,29 +70,161 @@ class StatisticsService {
       livestockConditionStats: {},
     };
 
-    // Group animals by farmId
     animals.forEach(animal => {
-      // Increment total count for the farm
       farmStats.total += 1;
 
       if (animal.gender === 'MALE') {
         farmStats.totalMale += 1;
 
-        // Phase statistics for male animals
         farmStats.malePhaseStats[animal.phase] = (farmStats.malePhaseStats[animal.phase] || 0) + 1;
       } else if (animal.gender === 'FEMALE') {
         farmStats.totalFemale += 1;
 
-        // Phase statistics for female animals
         farmStats.femalePhaseStats[animal.phase] = (farmStats.femalePhaseStats[animal.phase] || 0) + 1;
       }
 
-      // Livestock condition (status) statistics
       farmStats.livestockConditionStats[animal.status] = (farmStats.livestockConditionStats[animal.status] || 0) + 1;
     });
 
-    // Return statistics for the specific farm
     return farmStats;
+  }
+
+  async getAnimalCategorySummaryLactationData(category) {
+    try {
+      const animals = await Animal.findAll({
+        where: {
+          category,
+        },
+        include: [
+          {
+            model: LactationData,
+            as: 'lactationData',
+            include: [
+              {
+                model: YearlyData,
+                as: 'yearlyDatas',
+                include: [
+                  {
+                    model: MonthlyData,
+                    as: 'monthlyDatas',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        order: [
+          ['category', 'ASC'],
+        ],
+      });
+  
+      const summary = {
+        yearlyData: [],
+      };
+  
+      animals.forEach((animal) => {
+        const { lactationData } = animal;
+        console.log(animal)
+        console.log(lactationData)
+        if (!lactationData) return;
+  
+        lactationData.yearlyDatas.forEach((yearlyData) => {
+          let yearEntry = summary.yearlyData.find(
+            (entry) => entry.year === yearlyData.year
+          );
+  
+          if (!yearEntry) {
+            yearEntry = {
+              year: yearlyData.year,
+              data: Array.from({ length: 12 }, (_, i) => ({
+                month: new Date(0, i).toLocaleString('default', { month: 'short' }),
+                value: 0,
+              })),
+            };
+            summary.yearlyData.push(yearEntry);
+          }
+  
+          yearlyData.monthlyDatas.forEach((monthlyData) => {
+            const monthIndex = new Date(`${monthlyData.month} 1`).getMonth();
+            yearEntry.data[monthIndex].value += monthlyData.value;
+          });
+        });
+      });
+  
+      return summary;
+    } catch (error) {
+      console.error('Error fetching animal category summary:', error);
+      throw new Error('Could not fetch animal category summary.');
+    }
+  }
+  
+  async getAnimalCategorySummaryMilkData(category) {
+    try {
+      const animals = await Animal.findAll({
+        where: {
+          category,
+        },
+        include: [
+          {
+            model: MilkData,
+            as: 'milkData',
+            include: [
+              {
+                model: YearlyData,
+                as: 'yearlyDatas',
+                include: [
+                  {
+                    model: MonthlyData,
+                    as: 'monthlyDatas',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        order: [
+          ['category', 'ASC'],
+        ],
+      });
+  
+      const summary = {
+        yearlyData: [],
+      };
+  
+      animals.forEach((animal) => {
+        const { milkData } = animal;
+        console.log(animal)
+        console.log(milkData)
+        if (!milkData) return;
+  
+        milkData.yearlyDatas.forEach((yearlyData) => {
+          let yearEntry = summary.yearlyData.find(
+            (entry) => entry.year === yearlyData.year
+          );
+  
+          if (!yearEntry) {
+            yearEntry = {
+              year: yearlyData.year,
+              data: Array.from({ length: 12 }, (_, i) => ({
+                month: new Date(0, i).toLocaleString('default', { month: 'short' }),
+                value: 0,
+              })),
+            };
+            summary.yearlyData.push(yearEntry);
+          }
+  
+          yearlyData.monthlyDatas.forEach((monthlyData) => {
+            const monthIndex = new Date(`${monthlyData.month} 1`).getMonth();
+            yearEntry.data[monthIndex].value += monthlyData.value;
+          });
+        });
+      });
+  
+      return summary;
+    } catch (error) {
+      console.error('Error fetching animal category summary:', error);
+      throw new Error('Could not fetch animal category summary.');
+    }
   }
 }
 
